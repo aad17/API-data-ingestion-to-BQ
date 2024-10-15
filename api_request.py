746a361cd498c735c858a251a70ad96fa814a553
Deshingkar <ADESHING@jaguarlandrover.com>
2024-10-15 12:53:12 +0530

    Changed the code to append the api result to csv

--- api_request.py
+++ api_request.py
@@ -11,14 +11,21 @@ try:
     response = requests.get(api_url, verify=False)
     response.raise_for_status()  # Check if the request was successful
     data = response.json()  # Parse the JSON response
-    print(data)
+    # Normalize JSON data
+    normalised_data = pd.json_normalize(data)
+
+    # Convert JSON to oandas dataframe
+    df = pd.DataFrame(normalised_data)
+
+    # Define the CSV file path
+    csv_file = 'weather_data.csv'
+
+    # Check if the file exists to determine if the header should be written
+    if not os.path.isfile(csv_file):
+        df.to_csv(csv_file, index=False)
+    else:
+        df.to_csv(csv_file, mode='a', header=False, index=False)
 except requests.exceptions.HTTPError as http_err:
     print(f"HTTP error occurred: {http_err}")
 except Exception as err:
     print(f"Other error occurred: {err}")
-
-normalised_data = pd.json_normalize(data)
-df = pd.DataFrame(normalised_data)
-print(df)
-df.to_csv('weather_data.csv', index=False)
-print("Data has been stored in weather_data.csv")
